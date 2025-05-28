f512ac55b27ad78000837bf171936f98529b6db2
SweetlyBossy <sara.al.ya@hotmail.com>
2025-05-28 23:18:12 +0200

    starting update to the assigment
    creating all of the required files and assets, started on the code of the pages

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+const projects = [
+    {
+        title: 'EXAM PROJECT 1',
+        description:{
+            main: 'This is the project exam 1 assigment at Noroff. This is about a blog where the client wishes to create a blog spot for all wine enthusiasts. This is about a sommelier that is retired with 30+ years of experience in her line of work. Where the client wishes to share her knowledge, experiences and her adventures.',
+            short: 'A retired sommelier shares 30+ years of wine expertise, experiences, and adventures through a blog for wine enthusiasts.',
+            techMain: 'In this project I was able to use HTML, CSS and Javascript. I used VS code as the editor tool.I used Wave and lighthouse to keep track of all the different SEOs and performance metrics.I used sqoouosh to be able to scale down the file size of all images, before uploading to imgHippo.I used imgHippo to host my images to get a url for them to use.I used the Noroff API to interact with.',
+            techShort: 'HTML, CSS, Javascript, VS code, Wave, LightHouse, Sqooush, ImgHippo, Noroff API',
+        },
+        media: {
+            url: 'https://i.imghippo.com/files/Lx3878IJI.png',
+            alt: 'The logo for FED1-PE1 Wine Whisperer assignment',
+        },
+
+    }
+]
