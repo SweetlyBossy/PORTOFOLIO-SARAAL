6661e5e2c1919d5ba35163f5719a8537acc2e5b7
ReZvanYY <sara.al.ya@hotmail.com>
2025-05-29 12:04:59 +0200

    code for 3 latest projects is completed

---
+++ JS/index.js
@@ -0,0 +1,23 @@
+import { projects } from './projects.js';
+
+function displayProjectCards () {
+    const displayArea = document.querySelector('.latest-project-container');
+    displayArea.innerHTML = '';
+    projects.forEach(projects => {
+        const projectCards = document.createElement('article');
+        projectCards.classList.add('project-card');
+        projectCards.innerHTML = `
+        <h2 class="project-name">${projects.title}</h2>
+        <h5 class="project-name-smaller">${projects.subTitle}<h5>
+        <img src="${projects.media.url}" alt="${projects.media.alt}" class="project-img"/>
+        <p class="short-description">${projects.description.short}</p>
+
+        <div class="button-container">
+        <a href="${projects.link.githubRepoLink}"><button class="project-button">GITHUB REPO</button></a>
+        <a href="${projects.link.deploymentLink}"><button class="project-button">DEPLOYMENT LINK</button></a>
+        </div>
+        `;
+        displayArea.appendChild(projectCards);
+    })
+}
+displayProjectCards();
--- JS/projects.js
+++ JS/projects.js
@@ -1,6 +1,8 @@
-const projects = [
+export const projects = [
     {
-        title: 'EXAM PROJECT 1',
+        
+        title:'Whine Whisperer',
+        subTitle: 'PROJECT EXAM 1',
         description:{
             main: 'This is the project exam 1 assigment at Noroff. This is about a blog where the client wishes to create a blog spot for all wine enthusiasts. This is about a sommelier that is retired with 30+ years of experience in her line of work. Where the client wishes to share her knowledge, experiences and her adventures.',
             short: 'A retired sommelier shares 30+ years of wine expertise, experiences, and adventures through a blog for wine enthusiasts.',
@@ -9,20 +11,47 @@ const projects = [
         },
         media: {
             url: 'https://i.imghippo.com/files/Lx3878IJI.png',
-            alt: 'The logo for FED1-PE1 Wine Whisperer assignment',
+            alt: 'The logo project exam 1 Wine Whisperer assignment',
         },
-        title: '',
+        link: {
+            githubRepoLink: 'https://github.com/SweetlyBossy/FED-PE1-SaraAL',
+            deploymentLink: 'https://main--noroff-fed1-pe1-saraal.netlify.app/',
+        },
+    },
+    {
+        title:'Science Museum of London',
+        subTitle: 'SEMESTER PROJECT 1',
         description:{
-            main: '',
-            short: '',
-            techMain: '',
-            techShort: 'HTML, CSS, Javascript, VS code, Wave, LightHouse, Sqooush,',
+            main: 'This is for the semester project 1 assigment in Noroff. Here I had to design and build a whole web application for a science museum. The museum had to be for children between the age of 7 to 17 years old, as the target audience. The goal of the web application was to create more engangment with this community science museum for the youth in the area.',
+            short: 'Designed and built a science museum web app for engaging children aged 7–17, promoting community involvement and interactive learning.',
+            techMain: 'Microsoft Visual code Figma - for design and wireframe. Design.com - a new logo. Sqooush.app - for reducing file size and resizing images. Wave - To check for WCAG standard compliance. W3 Validator CSS- to validate CSS. W3 Validator - To validate HTML. Google Lightouse - Used for checking performance and SEO statistic.',
+            techShort: 'HTML, CSS, Javascript, VS code, Wave, LightHouse, Sqooush, Figma, Design.com, W3',
         },
         media: {
-            url: 'https://i.imghippo.com/files/Lx3878IJI.png',
-            alt: 'The logo for FED1-PE1 Wine Whisperer assignment',
+            url: 'https://i.imghippo.com/files/bHMj8884yj.png',
+            alt: 'The logo for semester project 1 Science Museum of London assigment',
         },
-        
-
+        link: {
+            githubRepoLink: 'https://github.com/SweetlyBossy/Semester-Project-1',
+            deploymentLink: 'https://sweetlybossy.github.io/Semester-Project-1/',
+        },
+    },
+    {
+        title:'GameHub',
+        subTitle: 'CROSS COURSE PROJECT 1',
+        description:{
+            main: 'This is for the cross course project 1 assigment in Noroff. Here the task was to design and build a web application with only HTML and CSS. This web app is for younger gamers around 18 to 25 years old. GameHub is supposed to take games and make them available for a competitive price. Everything was build from scratch on this project. The goal was to make it look like a real game store.',
+            short: 'Designed and built GameHub, a mock game store web app for gamers aged 18–25, using only HTML and CSS from scratch',
+            techMain: 'Used Figma to create a design and wireframe. coding languages was HTML and CSS. Used VS code as code editor. Wave, Google Lighthouse, W3 to confirm and validate the whole page. Sqooush for image optimization. Design.com for logo creation.',
+            techShort: 'HTML, CSS, VS code, Wave, LightHouse, Sqooush, Figma, Design.com, W3',
+        },
+        media: {
+            url: 'https://i.imghippo.com/files/SNMQ9268plk.png',
+            alt: 'The logo for cross Course project 1 GameHub assignment',
+        },
+        link: {
+            githubRepoLink: 'https://github.com/SweetlyBossy/HTML-CSS-CA-Sara-AL',
+            deploymentLink: 'https://sweetlybossy.github.io/HTML-CSS-CA-Sara-AL/',
+        },
     }
 ]
